20955db27d83728343091cea24c393e54d83fc77
Juan Paulo Passi <passipaulo14@gmail.com>
2024-10-12 10:30:37 +0800

    Create taskCalendar.js

---
+++ taskCalendar.js
@@ -0,0 +1,21 @@
+let day = prompt("Enter a day in a week:");
+
+switch (day) {
+    case "Monday":
+        alert("You have a work out today.");
+        break;
+    case "Tuesday":
+        alert("Do your assignments.");
+        break;
+    case "Wednesday":
+        alert("Review your lessons.");
+        break;
+    case "Thursday":
+        alert("You have a P.E practice today.");
+        break;
+    case "Friday":
+        alert("You have a outdoor activity today.");
+        break;
+    default:
+        alert("Saturday or Sunday is rest and family day.");
+}
